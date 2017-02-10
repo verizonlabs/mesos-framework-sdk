4cd504bd0abb4ac964579fc288d41da7644831e1
Tim Hansen <timothy.hansen@one.verizon.com>
2017-02-10 12:41:57 -0500

    Add base protofiles.

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package mesos_framework_sdk
 
+import (
+	exec "mesos-framework-sdk/include/executor"
+	sched "mesos-framework-sdk/include/scheduler"
+	mesos "mesos-framework-sdk/include/mesos"
+)
 func main(){
 
 }
