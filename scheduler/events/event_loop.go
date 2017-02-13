4b2609d66cd8f93350f11c9a52e98c3c2e8cc0f3
Aaron Wood <aaronjwood@gmail.com>
2017-02-13 18:14:18 -0500

    Log before continuing

--- scheduler/events/event_loop.go
+++ scheduler/events/event_loop.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"github.com/golang/protobuf/proto"
 	"io"
+	"log"
 	sched "mesos-framework-sdk/include/scheduler"
 	"strconv"
 	"strings"
@@ -16,22 +17,26 @@ func Loop(data io.ReadCloser, events chan<- *sched.Event) {
 	for {
 		lengthStr, err := reader.ReadString('\n')
 		if err != nil {
+			log.Println("Failed to read length prefix from RecordIO: " + err.Error())
 			continue
 		}
 
 		lengthInt, err := strconv.Atoi(strings.TrimRight(lengthStr, "\n"))
 		if err != nil {
+			log.Println("Failed to convert the message length: " + err.Error())
 			continue
 		}
 
 		buffer := make([]byte, lengthInt)
 		n, err := io.ReadFull(reader, buffer)
 		if n != lengthInt {
+			log.Println("Failed to read the RecordIO message: " + err.Error())
 			continue
 		}
 
 		err = proto.Unmarshal(buffer, &event)
 		if err != nil {
+			log.Println("Failed to unmarshal event: " + err.Error())
 			continue
 		}
 
