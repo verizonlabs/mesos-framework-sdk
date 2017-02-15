f3fbfada49ef246b38b52e6bf8121e67f46e8a22
Tim Hansen <timothy.hansen@one.verizon.com>
2017-02-15 12:34:29 -0500

    Add listen method to executor. Add channel to executor.

--- executor/executor.go
+++ executor/executor.go
@@ -4,8 +4,10 @@ package executor
 Executor interface and default executor implementation is defined here.
 */
 import (
+	"fmt"
 	"github.com/golang/protobuf/proto"
 	"mesos-framework-sdk/client"
+	"mesos-framework-sdk/executor/events"
 	exec "mesos-framework-sdk/include/executor"
 	"mesos-framework-sdk/include/mesos"
 )
@@ -14,6 +16,7 @@ type Executor interface {
 	FrameworkID() *mesos_v1.FrameworkID
 	ExecutorID() *mesos_v1.ExecutorID
 	Client() *client.Client
+	Events() chan *exec.Event
 	Subscribe()
 	Update(taskStatus *mesos_v1.TaskStatus)
 	Message(data []byte)
@@ -23,15 +26,54 @@ type DefaultExecutor struct {
 	frameworkID *mesos_v1.FrameworkID
 	executorID  *mesos_v1.ExecutorID
 	client      *client.Client
+	events      chan *exec.Event
+	handlers    events.ExecutorEvents
 }
 
-func NewDefaultExecutor(master string) *DefaultExecutor {
+// Creates a new default executor
+func NewDefaultExecutor(c *client.Client) *DefaultExecutor {
 	return &DefaultExecutor{
 		frameworkID: &mesos_v1.FrameworkID{Value: proto.String("")},
 		executorID:  &mesos_v1.ExecutorID{Value: proto.String("")},
-		client:      client.NewClient(master),
+		client:      c,
+		events:      make(chan *exec.Event),
 	}
 
+}
+
+// Default listening method on the
+func (d *DefaultExecutor) listen() {
+	for {
+		switch t := <-d.events; t.GetType() {
+		case exec.Event_SUBSCRIBED:
+			go d.handlers.Subscribed()
+			break
+		case exec.Event_ACKNOWLEDGED:
+			go d.handlers.Acknowledged()
+			break
+		case exec.Event_MESSAGE:
+			go d.handlers.Message()
+			break
+		case exec.Event_KILL:
+			go d.handlers.Kill()
+			break
+		case exec.Event_LAUNCH:
+			go d.handlers.Launch()
+			break
+		case exec.Event_LAUNCH_GROUP:
+			go d.handlers.LaunchGroup()
+			break
+		case exec.Event_SHUTDOWN:
+			go d.handlers.Shutdown()
+			break
+		case exec.Event_ERROR:
+			go d.handlers.Error()
+			break
+		case exec.Event_UNKNOWN:
+			fmt.Println("Unknown event caught.")
+			break
+		}
+	}
 }
 
 func (d *DefaultExecutor) FrameworkID() {
