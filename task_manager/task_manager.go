25371343d870784488a1252af6481076a41d9140
Aaron Wood <aaronjwood@gmail.com>
2017-03-03 22:47:55 -0500

    Use the default zero initialization for totalTasks and don't hardcode 1 when creating a new map

--- task_manager/task_manager.go
+++ task_manager/task_manager.go
@@ -24,8 +24,7 @@ type DefaultTaskManager struct {
 
 func NewDefaultTaskManager() *DefaultTaskManager {
 	return &DefaultTaskManager{
-		tasks:         structures.NewConcurrentMap(1),
-		totalTasks:    0,
+		tasks:         structures.NewConcurrentMap(),
 		launchedTasks: make(map[string]*mesos_v1.TaskInfo),
 		queuedTasks:   make(map[string]*mesos_v1.TaskInfo),
 	}
