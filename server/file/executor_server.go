0bac4a874488abe2c455fb075fb27914d20eebc6
Tim Hansen <timothy.hansen@one.verizon.com>
2017-03-06 17:14:19 -0500

    Remove hardcoded IP.

--- server/file/executor_server.go
+++ server/file/executor_server.go
@@ -47,7 +47,7 @@ func (s *executorServer) Serve() {
 
 	if s.cfg.TLS() {
 		s.cfg.Server().Handler = s.mux
-		s.cfg.Server().Addr = "68.128.155.54:" + strconv.Itoa(s.cfg.Port())
+		s.cfg.Server().Addr = ":" + strconv.Itoa(s.cfg.Port())
 		fmt.Printf("%v", s.cfg.Server().Addr)
 		log.Fatal(s.cfg.Server().ListenAndServeTLS(s.cfg.Cert(), s.cfg.Key()))
 	} else {
