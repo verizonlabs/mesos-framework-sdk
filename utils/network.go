c8dddc0c65c1a76444a43b5eb0d2328717123ed2
Aaron Wood <aaronjwood@gmail.com>
2017-03-20 15:55:02 -0400

    Add network utils

---
+++ utils/network.go
@@ -0,0 +1,39 @@
+package utils
+
+import "net"
+
+// Gets the IPs corresponding to the specified interface.
+// This can be used for when you need to determine your network state for leader election, etc.
+func GetIP(iface string) ([]string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	ips := make([]net.IP, 0)
+
+	for _, i := range ifaces {
+		if i.Name != iface {
+			continue
+		}
+
+		addrs, err := i.Addrs()
+		if err != nil {
+			return "", err
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			ips = append(ips, ip.String())
+		}
+	}
+
+	return ips, err
+}
