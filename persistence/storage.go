b9dd4ddbbcddba94a2b86bce337980c6608c994f
Aaron Wood <aaronjwood@gmail.com>
2017-03-28 14:18:59 -0400

    Formatting

--- persistence/storage.go
+++ persistence/storage.go
@@ -11,6 +11,6 @@ type Storage interface {
 	Read(...string) ([]string, error)
 	Update(string, ...string) error
 	Delete(string, ...string) error
-	Driver() string // There is no "setter" because we declare a concrete driver type
+	Driver() string      // There is no "setter" because we declare a concrete driver type
 	Engine() interface{} // Pass back an instance of the engine client.
 }
