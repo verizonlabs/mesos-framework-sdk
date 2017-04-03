ec5eb7d5cc3a676ad80d047062700aaa7cb45808
Aaron Wood <aaronjwood@gmail.com>
2017-04-03 16:28:51 -0400

    Move the default executor implementation here and handle communication with Mesos for everyone

--- executor/executor.go
+++ executor/executor.go
@@ -1,12 +1,80 @@
 package executor
 
 import (
+	"mesos-framework-sdk/client"
 	exec "mesos-framework-sdk/include/executor"
 	"mesos-framework-sdk/include/mesos"
+	"mesos-framework-sdk/logging"
+	"mesos-framework-sdk/recordio"
 )
 
 type Executor interface {
 	Subscribe(chan *exec.Event) error
 	Update(*mesos_v1.TaskStatus)
 	Message([]byte)
+}
+
+type DefaultExecutor struct {
+	FrameworkID *mesos_v1.FrameworkID
+	ExecutorID  *mesos_v1.ExecutorID
+	client      client.Client
+	logger      logging.Logger
+}
+
+// Creates a new default executor
+func NewDefaultExecutor(
+	f *mesos_v1.FrameworkID,
+	e *mesos_v1.ExecutorID,
+	c client.Client,
+	lgr logging.Logger) Executor {
+
+	return &DefaultExecutor{
+		FrameworkID: f,
+		ExecutorID:  e,
+		client:      c,
+		logger:      lgr,
+	}
+}
+
+func (e *DefaultExecutor) Subscribe(eventChan chan *exec.Event) error {
+	subscribe := &exec.Call{
+		FrameworkId: e.FrameworkID,
+		ExecutorId:  e.ExecutorID,
+		Type:        exec.Call_SUBSCRIBE.Enum(),
+	}
+
+	// If we disconnect we need to reset the stream ID. For this reason always start with a fresh stream ID.
+	// Otherwise we'll never be able to reconnect.
+	e.client.SetStreamID("")
+
+	resp, err := e.client.Request(subscribe)
+	if err != nil {
+		return err
+	} else {
+		return recordio.Decode(resp.Body, eventChan)
+	}
+}
+
+func (e *DefaultExecutor) Update(taskStatus *mesos_v1.TaskStatus) {
+	update := exec.Call{
+		FrameworkId: e.FrameworkID,
+		ExecutorId:  e.ExecutorID,
+		Type:        exec.Call_UPDATE.Enum(),
+		Update: &exec.Call_Update{
+			Status: taskStatus,
+		},
+	}
+	e.client.Request(update)
+}
+
+func (e *DefaultExecutor) Message(data []byte) {
+	message := exec.Call{
+		FrameworkId: e.FrameworkID,
+		ExecutorId:  e.ExecutorID,
+		Type:        exec.Call_MESSAGE.Enum(),
+		Message: &exec.Call_Message{
+			Data: data,
+		},
+	}
+	e.client.Request(message)
 }
