7c862adc90c943d0a7a999fe74c882add430efed
Aaron Wood <aaronjwood@gmail.com>
2017-04-03 16:28:16 -0400

    Remove default executor controller implementation and adjust interface a bit

--- executor/events/events.go
+++ executor/events/events.go
@@ -1,7 +1,6 @@
 package events
 
 import (
-	"fmt"
 	"mesos-framework-sdk/include/executor"
 )
 
@@ -24,37 +23,5 @@ type ExecutorEvents interface {
 	Message(*mesos_v1_executor.Event_Message)
 	Shutdown()
 	Error(*mesos_v1_executor.Event_Error)
-}
-
-type DefaultExecutorEvents struct {
-}
-
-// TODO replace fmt with our logging interface
-
-func (d *DefaultExecutorEvents) Subscribed(sub *mesos_v1_executor.Event_Subscribed) {
-	sub.GetFrameworkInfo()
-}
-
-func (d *DefaultExecutorEvents) Launch(launch *mesos_v1_executor.Event_Launch) {
-	fmt.Println(launch.GetTask())
-}
-
-func (d *DefaultExecutorEvents) LaunchGroup(launchGroup *mesos_v1_executor.Event_LaunchGroup) {
-	fmt.Println(launchGroup.GetTaskGroup())
-}
-
-func (d *DefaultExecutorEvents) Kill(kill *mesos_v1_executor.Event_Kill) {
-	fmt.Printf("%v, %v\n", kill.GetTaskId(), kill.GetKillPolicy())
-}
-func (d *DefaultExecutorEvents) Acknowledged(acknowledge *mesos_v1_executor.Event_Acknowledged) {
-	fmt.Printf("%v\n", acknowledge.GetTaskId())
-}
-func (d *DefaultExecutorEvents) Message(message *mesos_v1_executor.Event_Message) {
-	fmt.Printf("%v\n", message.GetData())
-}
-func (d *DefaultExecutorEvents) Shutdown() {
-
-}
-func (d *DefaultExecutorEvents) Error(error *mesos_v1_executor.Event_Error) {
-	fmt.Printf("%v\n", error.GetMessage())
+	Run()
 }
