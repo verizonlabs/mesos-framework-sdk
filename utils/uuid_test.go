53cad380a971c9339a64fbf40b202424707459fc
Aaron Wood <aaronjwood@gmail.com>
2017-04-04 11:53:27 -0400

    Add test to check if our UUID is a valid v4 UUID or not

--- utils/uuid_test.go
+++ utils/uuid_test.go
@@ -1,11 +1,35 @@
 package utils
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
-// Make sure we return a valid v4 UUID with the proper length.
+// Make sure we return a UUID with the proper length.
 func TestUuid(t *testing.T) {
+	t.Parallel()
+
 	u := Uuid()
 	if len(u) != 16 {
 		t.Fatal("The length of the UUID is incorrect")
+	}
+}
+
+// Checks if we have a valid v4 UUID.
+func TestUuidToString(t *testing.T) {
+	t.Parallel()
+
+	u, err := UuidToString(Uuid())
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	m, err := regexp.MatchString("^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$", u)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if !m {
+		t.Fatal("Not a valid v4 UUID")
 	}
 }
