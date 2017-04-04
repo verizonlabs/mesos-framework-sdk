2771cdcd9842d4e4bfcde084614733eae45691ca
Aaron Wood <aaronjwood@gmail.com>
2017-04-04 11:44:35 -0400

    Break out test for Emit

--- logging/logging_test.go
+++ logging/logging_test.go
@@ -2,12 +2,21 @@ package logging
 
 import "testing"
 
+// Ensure we can use our default logger.
 func TestNewDefaultLogger(t *testing.T) {
+	t.Parallel()
+
 	l := NewDefaultLogger()
 	if _, ok := l.(Logger); !ok {
 		t.Fatal("Default logger is of the wrong type")
 	}
+}
 
+// Tests if there are any issues with emitting messages.
+func TestDefaultLogger_Emit(t *testing.T) {
+	t.Parallel()
+
+	l := NewDefaultLogger()
 	// Test code path.
 	l.Emit(EVENT, "TEST %s", "VALUE")
 }
