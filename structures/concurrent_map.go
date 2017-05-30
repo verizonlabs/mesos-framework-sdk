34dc2cd16d3137a2f56889d0a4ab1cb63e814873
Aaron Wood <aaronjwood@gmail.com>
2017-05-30 11:13:36 -0400

    Fix edge case where there could be a race between making the channel with a specified size and iterating on the internal map to fill the channel

--- structures/concurrent_map.go
+++ structures/concurrent_map.go
@@ -64,20 +64,20 @@ func (c *ConcurrentMap) Delete(key interface{}) {
 // Safely iterates over the map.
 // Provides the key/values to a channel that is returned for use by the client.
 func (c *ConcurrentMap) Iterate() <-chan Item {
-	ch := make(chan Item, c.Length())
+	c.RLock()
+	ch := make(chan Item, len(c.data))
 
 	go func() {
-		c.RLock()
 		for key, value := range c.data {
 			ch <- Item{
 				Key:   key,
 				Value: value,
 			}
 		}
-		c.RUnlock()
 
 		close(ch)
 	}()
+	c.RUnlock()
 
 	return ch
 }
