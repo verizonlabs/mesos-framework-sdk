db9721ee082c1a427e777ebb857efcc0ad7201f1
Aaron Wood <aaronjwood@gmail.com>
2017-07-27 14:49:07 -0400

    Adjust mock so that we can more easily detect if any encoded task data is not being generated and written to the data store

--- persistence/drivers/etcd/test/MockEtcd.go
+++ persistence/drivers/etcd/test/MockEtcd.go
@@ -4,20 +4,37 @@ import "errors"
 
 type MockKVStore struct{}
 
-func (m MockKVStore) Create(key, value string) error {
+func validateData(key, value string) error {
+
+	// Skip validating the leader since we want this to be empty to break out of our HA loop.
+	if key == "/leader" {
+		return nil
+	}
+
+	if key == "" {
+		return errors.New("No key was defined")
+	}
+	if value == "" {
+		return errors.New("No data was written")
+	}
+
 	return nil
 }
+
+func (m MockKVStore) Create(key, value string) error {
+	return validateData(key, value)
+}
 func (m MockKVStore) CreateWithLease(key, value string, ttl int64) (int64, error) {
-	return 0, nil
+	return 0, validateData(key, value)
 }
 func (m MockKVStore) Read(key string) (string, error) {
 	return "1", nil
 }
 func (m MockKVStore) ReadAll(key string) (map[string]string, error) {
-	return map[string]string{"key": "value"}, nil
+	return map[string]string{}, nil
 }
 func (m MockKVStore) Update(key, value string) error {
-	return nil
+	return validateData(key, value)
 }
 func (m MockKVStore) RefreshLease(id int64) error {
 	return nil
@@ -37,10 +54,10 @@ func (m MockBrokenKVStore) CreateWithLease(key, value string, ttl int64) (int64,
 	return 0, brokenStorage
 }
 func (m MockBrokenKVStore) Read(key string) (string, error) {
-	return "", brokenStorage
+	return "1", brokenStorage
 }
 func (m MockBrokenKVStore) ReadAll(key string) (map[string]string, error) {
-	return map[string]string{"key": "value"}, brokenStorage
+	return map[string]string{}, brokenStorage
 }
 func (m MockBrokenKVStore) Update(key, value string) error {
 	return brokenStorage
@@ -55,19 +72,19 @@ func (m MockBrokenKVStore) Delete(key string) error {
 type MockEtcd struct{}
 
 func (m MockEtcd) Create(key, value string) error {
-	return nil
+	return validateData(key, value)
 }
 func (m MockEtcd) CreateWithLease(key, value string, ttl int64) (int64, error) {
-	return 0, nil
+	return 0, validateData(key, value)
 }
 func (m MockEtcd) Read(key string) (string, error) {
 	return "1", nil
 }
 func (m MockEtcd) ReadAll(key string) (map[string]string, error) {
-	return map[string]string{"key": "value"}, nil
+	return map[string]string{}, nil
 }
 func (m MockEtcd) Update(key, value string) error {
-	return nil
+	return validateData(key, value)
 }
 func (m MockEtcd) RefreshLease(id int64) error {
 	return nil
