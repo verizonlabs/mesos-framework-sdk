8fbfa20d3331e4860a9fdab31b00db322dc83fbe
Aaron Wood <aaronjwood@gmail.com>
2017-07-28 15:06:12 -0400

    Return array literal with resources

--- task/resources/resource.go
+++ task/resources/resource.go
@@ -8,19 +8,19 @@ import (
 )
 
 func ParseResources(res *task.ResourceJSON) ([]*mesos_v1.Resource, error) {
-	r := make([]*mesos_v1.Resource, 0)
+
 	// We require at least some cpu and some mem.
 	if res.Cpu <= 0.00 || res.Mem <= 0.00 {
-		return nil, errors.New("CPU or Memory must be greater than 0.0. " +
+		return nil, errors.New("CPU and memory must be greater than 0.0. " +
 			"Please make sure you set cpu and mem properly.")
 	}
 
-	var cpu = resources.CreateResource("cpus", res.Role, res.Cpu)
-	var mem = resources.CreateResource("mem", res.Role, res.Mem)
-	var disk, err = resources.CreateDisk(res.Disk, res.Role)
+	cpu := resources.CreateResource("cpus", res.Role, res.Cpu)
+	mem := resources.CreateResource("mem", res.Role, res.Mem)
+	disk, err := resources.CreateDisk(res.Disk, res.Role)
 	if err != nil {
 		return nil, err
 	}
-	r = append(r, cpu, mem, disk)
-	return r, nil
+
+	return []*mesos_v1.Resource{cpu, mem, disk}, nil
 }
