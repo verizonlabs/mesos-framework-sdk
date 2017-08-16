2c642161af928122d48ac6992b0d4e318c020b09
Aaron Wood <aaronjwood@gmail.com>
2017-08-16 16:06:32 -0400

    Add license header

--- client/client.go
+++ client/client.go
@@ -1,9 +1,22 @@
+// Copyright 2017 Verizon
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package client
 
 import (
 	"bytes"
 	"errors"
-	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 	"mesos-framework-sdk/include/mesos_v1_executor"
 	"mesos-framework-sdk/include/mesos_v1_scheduler"
@@ -12,6 +25,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/golang/protobuf/proto"
 )
 
 type Client interface {
